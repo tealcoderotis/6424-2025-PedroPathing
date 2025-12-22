6945eb93dbe2bb761e430c495294eedae9ecfdfe
Zach Hagelberg <zachhagelberg7@gmail.com>
2025-12-22 09:52:45 -0800

    New "RegressionBuilder" teleop.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RegressionBuilder.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.pedropathing.follower.Follower;
+
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@TeleOp(name = "RegressionBuilder")
+public class RegressionBuilder extends OpMode {
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotorEx launcher = null;
+    private DcMotorEx feeder = null;
+    private Follower follower;
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
+    boolean lockOn = false;
+    final double FEEDER_INTAKE_VELOCITY = 1600;
+    double launchVelocity = 1500;
+
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftBackDrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBackDrive");
+        launcher = hardwareMap.get(DcMotorEx.class, "launcher");
+        feeder = hardwareMap.get(DcMotorEx.class, "feeder");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        launcher.setDirection(DcMotor.Direction.REVERSE);
+        feeder.setDirection(DcMotor.Direction.REVERSE);
+
+        launcher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFrontDrive.setZeroPowerBehavior(BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(BRAKE);
+        leftBackDrive.setZeroPowerBehavior(BRAKE);
+        rightBackDrive.setZeroPowerBehavior(BRAKE);
+        launcher.setZeroPowerBehavior(BRAKE);
+
+        feeder.setPower(0);
+
+        launcher.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(300, 0, 0, 10));
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void init_loop() {
+        /*if (Globals.alliance != Alliance.UNKNOWN) {
+            alliance = Globals.alliance;
+            follower.setStartingPose(Globals.endingPose.copy());
+        }
+        else {
+            if (gamepad1.bWasPressed()) {
+                //Red starting pose
+                follower.setStartingPose(new Pose(125.200, 70.930, Math.toRadians(0)));
+                alliance = Alliance.RED;
+            } else if (gamepad1.xWasPressed()) {
+                //Blue starting pose
+                follower.setStartingPose(new Pose(46.892, 59.798, Math.toRadians(180)));
+                alliance = Alliance.BLUE;
+            }
+        }*/
+        follower.setStartingPose(new Pose(110.36335877862595, 134.10687022900763, Math.toRadians(0)));
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        follower.update();
+    }
+
+    @Override
+    public void start() {
+        follower.startTeleOpDrive(false);
+    }
+
+    @Override
+    public void loop() {
+        /*
+            How to use: First, set up where you set up red auton. Then move around to a position, and use
+            up, down, x, and y on gamepad2 to adjust shooter velocity. Find a good velocity.
+            Record the x value, y value, and a good shooter velocity to create a regression.
+        */
+        if (!lockOn) {
+            mecanumDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        } else {
+            double rotate = 0;
+            mecanumDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, rotate);
+        }
+        if (gamepad2.a) {
+            feeder.setDirection(DcMotor.Direction.FORWARD);
+            feeder.setVelocity(FEEDER_INTAKE_VELOCITY);
+            launcher.setDirection(DcMotor.Direction.FORWARD);
+            launcher.setVelocity(launchVelocity);
+        }
+        if (gamepad2.dpadUpWasPressed()) {
+            launchVelocity = launchVelocity + 10;
+        }
+        if (gamepad2.dpadDownWasPressed()) {
+            launchVelocity = launchVelocity - 10;
+        }
+        if (gamepad2.xWasPressed()) {
+            launchVelocity = launchVelocity + 100;
+        }
+        if (gamepad2.yWasPressed()) {
+            launchVelocity = launchVelocity - 100;
+        }
+        telemetry.addData("motorSpeed", launcher.getVelocity());
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        follower.update();
+    }
+    void mecanumDrive(double forward, double strafe, double rotate){
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontPower = (forward + strafe + rotate) / denominator;
+        rightFrontPower = (forward - strafe - rotate) / denominator;
+        leftBackPower = (forward - strafe + rotate) / denominator;
+        rightBackPower = (forward + strafe - rotate) / denominator;
+
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+
+    }
+}

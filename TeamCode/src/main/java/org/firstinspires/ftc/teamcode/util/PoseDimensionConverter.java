16b851fca117b651962fba71be264061d3e6f031
tealcoderotis <parallelcircuit17@gmail.com>
2025-12-22 10:08:22 -0800

    Switched angle unit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PoseDimensionConverter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PoseDimensionConverter.java
@@ -7,6 +7,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 public class PoseDimensionConverter {
     public static Pose2D pose3DToPose2D(Pose3D pose) {
-        return new Pose2D(pose.getPosition().unit, pose.getPosition().x, pose.getPosition().y, AngleUnit.DEGREES, pose.getOrientation().getYaw(AngleUnit.DEGREES));
+        return new Pose2D(pose.getPosition().unit, pose.getPosition().x, pose.getPosition().y, AngleUnit.RADIANS, pose.getOrientation().getYaw(AngleUnit.RADIANS));
     }
 }

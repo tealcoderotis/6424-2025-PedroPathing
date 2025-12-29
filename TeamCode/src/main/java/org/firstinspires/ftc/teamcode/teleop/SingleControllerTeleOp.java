58f5ddc5484e887050922c4d56864a9659175137
Zach Hagelberg <zachhagelberg7@gmail.com>
2025-12-29 12:21:09 -0800

    Single Controller TeleOp code added. No keybinds were changed, just all condensed to one controller as we have enough buttons.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleControllerTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleControllerTeleOp.java
@@ -8,12 +8,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Globals;
 import org.firstinspires.ftc.teamcode.ShooterMath;

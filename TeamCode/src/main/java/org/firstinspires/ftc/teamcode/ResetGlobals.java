b47cf0f298539254058f69e871ee472d5dc3fa2c
tealcoderotis <parallelcircuit17@gmail.com>
2025-12-29 13:57:42 -0800

    Added code to reset turret encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetGlobals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetGlobals.java
@@ -2,12 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "Reset Globals")
+@TeleOp(name = "Reset")
 public class ResetGlobals extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addLine("Press A to reset globals");
+            telemetry.addLine("Press B to reset turret encoder");
+            if (gamepad1.aWasPressed()) {
+                Globals.resetGlobals();
+                telemetry.addLine("Globals reset");
+            }
+            if (gamepad1.bWasPressed()) {
+                DcMotorEx turretRotate = (DcMotorEx)hardwareMap.get(Globals.TURRET_ROTATE_HARDWARE_MAP_NAME);
+                turretRotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                telemetry.addLine("Turret encoder reset");
+            }
+            telemetry.update();
+        }
         Globals.resetGlobals();
-        requestOpModeStop();
     }
 }

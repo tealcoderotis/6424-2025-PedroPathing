2393aac21467ebdc46b31a0ba775f744076fa540
Zach Hagelberg <zachhagelberg7@gmail.com>
2026-01-26 23:34:02 -0800

    2 separate potential ways to get limelight localization working, one via megatag 1 or 2, one via raw fiducial data about the corners of the apriltag

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/JSONfinder.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.util.Alliance;
+
+import java.util.List;
+
+@Configurable
+@TeleOp(name = "JSONfinder")
+public class JSONfinder extends OpMode {
+    private Limelight3A limelight;
+    private IMU imu;
+    private Alliance alliance = Alliance.UNKNOWN;
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(1); //1 check per second
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0); // only april tag 22
+        limelight.start();
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));//Initialize while facing right? maybe forward? Not 45 degrees.
+
+        telemetry.addData("Status", "Initialized");
+    }
+    @Override
+    public void init_loop() {
+        telemetry.update();
+    }
+    @Override
+    public void start() {
+        limelight.start();
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult result = limelight.getLatestResult();
+        if (result.isValid()) {
+            List<LLResultTypes.FiducialResult> results = result.getFiducialResults();
+            if (results != null) {
+                LLResultTypes.FiducialResult firstresult = results.get(0); //There should never be more than 1 result because it is filtered in the pipeline
+                if (firstresult != null) {
+                    List<List<Double>> targetcorners = firstresult.getTargetCorners();
+                    telemetry.addData("x0", targetcorners.get(0).get(0));
+                    telemetry.addData("y0", targetcorners.get(0).get(1));
+                    telemetry.addData("x1", targetcorners.get(1).get(0));
+                    telemetry.addData("y1", targetcorners.get(1).get(1));
+                    telemetry.addData("x2", targetcorners.get(2).get(0));
+                    telemetry.addData("y2", targetcorners.get(2).get(1));
+                    telemetry.addData("x3", targetcorners.get(3).get(0));
+                    telemetry.addData("y3", targetcorners.get(3).get(1));
+                }
+                else {
+                    telemetry.addLine("First Fiducial Result is null");
+                }
+            }
+            else {
+                telemetry.addLine("getFiducialResults returns null, have you 'enabled in output tab'?");
+            }
+        }
+        else {
+            telemetry.addLine("No valid apriltags");
+        }
+        telemetry.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/JustLocalization.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.util.Alliance;
+@Configurable
+@TeleOp(name = "JustLocalization")
+public class JustLocalization extends OpMode {
+    private Limelight3A limelight;
+    private Follower follower;
+    private IMU imu;
+    private Alliance alliance = Alliance.UNKNOWN;
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(5);
+        telemetry.setMsTransmissionInterval(11);
+        limelight.pipelineSwitch(0); // only april tag 22
+        limelight.start();
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD, RevHubOrientationOnRobot.UsbFacingDirection.LEFT);
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));//Initialize while facing right? maybe forward? Not 45 degrees.
+
+        telemetry.addData("Status", "Initialized");
+    }
+    @Override
+    public void init_loop() {
+        if (gamepad1.bWasPressed()) {
+            //Red starting pose
+            follower.setStartingPose(new Pose(125.200, 70.930, Math.toRadians(0)));
+            alliance = Alliance.RED;
+            limelight.pipelineSwitch(0); // only april tag 24
+            telemetry.addLine("Red alliance, tag 24");
+        } else if (gamepad1.xWasPressed()) {
+            //Blue starting pose
+            follower.setStartingPose(new Pose(46.892, 59.798, Math.toRadians(180)));
+            alliance = Alliance.BLUE;
+            limelight.pipelineSwitch(1); // only april tag 20
+            telemetry.addLine("Blue alliance, tag 20");
+        } else {
+            telemetry.addLine("ALLIANCE UNKNOWN");
+        }
+        telemetry.update();
+    }
+    @Override
+    public void start() {
+        limelight.start();
+    }
+    public static Pose pose3DToPedroPose(Pose3D pose) {
+        Pose2D pose2d = new Pose2D(pose.getPosition().unit, pose.getPosition().x, pose.getPosition().y, AngleUnit.RADIANS, pose.getOrientation().getYaw(AngleUnit.RADIANS));
+        Pose ftcStandard = PoseConverter.pose2DToPose(pose2d, InvertedFTCCoordinates.INSTANCE);
+        ftcStandard.getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+        return ftcStandard;
+    }
+    @Override
+    public void loop() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult result = limelight.getLatestResult();
+        if (result.isValid()){
+            Pose3D botposemt1 = result.getBotpose();//mt1
+            Pose3D botposemt2 = result.getBotpose_MT2();//mt2
+            Pose posemt1 = pose3DToPedroPose(botposemt1);
+            Pose posemt2 = pose3DToPedroPose(botposemt2);
+            telemetry.addData("Megatag1 Pose", posemt1.toString());
+            telemetry.addData("Megatag2 Pose", posemt2.toString());
+        } else {
+            telemetry.addLine("Result is not valid");
+        }
+
+    }
+}
